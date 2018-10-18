708ec80194db5e4347f283d77315592bac47c0f5
fclubb <f.clubb25@gmail.com>
2018-10-18 07:10:52 +0200

    Adding boxplot script for ERA5 climate data

--- clustering/plot_cluster_data.py
+++ clustering/plot_cluster_data.py
@@ -135,6 +135,7 @@ def ReadClimateData():
         this_df = df[df['RGIId'] == id]
         with open(DataDirectory+'/era5/{}.pkl'.format(id), 'rb') as f:
             ts = pickle.load(f)
+            # print(ts.columns)
             # mask to only include data within the landsat image times
             mask = ts.index >= '2013-03-31'
             ts = ts.loc[mask]
@@ -142,8 +143,88 @@ def ReadClimateData():
             # now get the mean temperature for this ID
             mean_temp = ts.tmean.mean()
             df.loc[df.RGIId == id, 'mean_temp'] = mean_temp
+            df.loc[df.RGIId == id, 'mean_pcp'] = ts.tp.mean()
+            df.loc[df.RGIId == id, 'mean_ws'] = ts.wsmean.mean()
+            df.loc[df.RGIId == id, 'mean_cc'] = ts.CCmean.mean()
 
-    print(ts.columns)
+    print(df)
+    df.to_csv(DataDirectory+'TSI+RGI+ERA5_clustered.csv')
+
+    clusters = df['cluster_id'].unique()
+    for cl in clusters:
+        this_df = df[df['cluster_id'] == cl]
+        print("Cluster {}, median temp = {}".format(cl, this_df['mean_temp'].median()))
+        print("Cluster {}, median pcp = {}".format(cl, this_df['mean_pcp'].median()))
+        print("Cluster {}, median wind speed = {}".format(cl, this_df['mean_ws'].median()))
+        print("Cluster {}, median cloud cover = {}".format(cl, this_df['mean_cc'].median()))
+    print("========================================")
+
+    # set props for fliers
+    flierprops = dict(marker='o', markerfacecolor='none', markersize=1,
+                  linestyle='none', markeredgecolor='k')
+
+    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(6,6))
+    axes = axes.ravel()
+    # make a big subplot to allow sharing of axis labels
+    fig.add_subplot(111, frameon=False)
+    # hide tick and tick label of the big axes
+    plt.tick_params(labelcolor='none', top='off', bottom='off', left='off', right='off')
+
+    col_keys = ['mean_temp', 'mean_pcp', 'mean_ws', 'mean_cc']
+    labels = ['Mean temperature ($\circ$C)', 'Mean precipitation (mm)', 'Mean wind speed', 'Mean cloud cover']
+
+    N_colors = 8
+    colors = list_of_hex_colours(N_colors, 'Set2')
+    for i, this_ax in enumerate(axes):
+        this_ax.set_ylabel(labels[i])
+        this_ax.set_xlabel('')
+        # make the boxplot and return the dict with the boxplot properties
+        bp_dict = df.boxplot(column=col_keys[i], by=['cluster_id'], return_type='both', patch_artist=True, flierprops=flierprops, ax=this_ax)
+        # make the median lines black
+        #[[item.set_color('k') for item in bp_dict[key]['medians']] for key in bp_dict.keys()]
+
+        # change the colours based on the cluster ID
+        for row_key, (ax,row) in bp_dict.iteritems():
+            this_ax.set_xlabel('')
+            j=-1 #stupid thing because there are double the number of caps and whiskers compared to boxes
+            for i,cp in enumerate(row['caps']):
+                if i%2==0:
+                    j+=1
+                # cp.set(color=colors[j])
+                cp.set(color='k')
+            j=-1
+            for i,wh in enumerate(row['whiskers']):
+                if i%2==0:
+                    j+=1
+                # wh.set_color(colors[j])
+                wh.set_color('k')
+            for i,box in enumerate(row['boxes']):
+                box.set_facecolor(colors[i])
+                box.set_alpha(0.7)
+                # box.set_edgecolor(colors[i])
+                box.set_edgecolor('k')
+            for i,med in enumerate(row['medians']):
+                med.set(color='k')
+            for i,pt in enumerate(row['fliers']):
+                pt.set_markeredgecolor(colors[i])
+
+
+        ax.grid(color='0.8', linestyle='--', which='major', zorder=1)
+        plt.subplots_adjust(wspace=0.3,left=0.25,right=0.9, hspace=0.35, bottom=0.1)
+        x_labels = [str((int(x))) for x in df.cluster_id.unique()]
+        ax.set_xticklabels(x_labels, fontsize=12)
+        #print(boxplot)
+    plt.suptitle('')
+    plt.xlabel('Cluster number')
+    plt.tight_layout()
+
+        # ax.set_ylabel('Catchment relief (m)', fontsize=14)
+
+    #plt.subplots_adjust(left=0.2)
+    plt.savefig(DataDirectory+'cluster_boxplot_climate.png', dpi=300)
+    plt.clf()
+
+
 
     #print(df)
 
@@ -176,6 +257,6 @@ def MakeProfilesByCluster():
     plt.savefig(DataDirectory+'subplots_clustered.png', dpi=300)
 
 DataDirectory = '../data/'
-MakeBoxPlotByCluster()
-#ReadClimateData()
+#MakeBoxPlotByCluster()
+ReadClimateData()
 #MakeProfilesByCluster()
